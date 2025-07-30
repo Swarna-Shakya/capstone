4ae2c0d7d875c764e961baf92e0ed3729b95b82a
Swarna-Shakya <swarnamanshakya@gmail.com>
2025-07-30 19:36:16 +0545

    [ADD] API to get available rooms in date range

--- api/v1/getAvailableRooms.php
+++ api/v1/getAvailableRooms.php
@@ -7,17 +7,34 @@ include_once '../../include/initialize.php';
 
 $request_method = $_SERVER['REQUEST_METHOD'];
 
-if ($request_method == 'GET') {
+if ($request_method == 'POST') {
+
+    http_response_code(500);
+    $post_data = json_decode(file_get_contents("php://input"));
+    if (empty($post_data->check_in)) {
+        http_response_code(400);
+        echo json_encode(array("action" => "error", "message" => "Check-in date is required."));
+        exit();
+    }
+    if (empty($post_data->check_out)) {
+        http_response_code(400);
+        echo json_encode(array("action" => "error", "message" => "Check-out date is required."));
+        exit();
+    }
+
+    $check_in = $post_data->check_in;
+    $check_out = $post_data->check_out;
+
     $rooms_data = [];
     // Fetch room data from the database
-    $query = "SELECT * FROM rooms ORDER BY id DESC";
-    $rooms = Rooms::find_by_sql($query);
-    if ($rooms) {
-        foreach ($rooms as $room) {
+    $available_rooms = Bookings::check_available_new($check_in, $check_out);
+    if ($available_rooms && $available_rooms->num_rows > 0) {
+        while ($room = $available_rooms->fetch_object()) {
             $room_data = [
-                'id' => $room->id,
-                'title' => $room->title,
+                'id' => $room->room_id,
+                'title' => $room->room_title,
                 'quantity' => $room->room_qnty,
+                'available_rooms' => $room->available_rooms,
                 'beds' => $room->beds,
                 'bed_type' => $room->bed_type,
                 'image' => BASE_URL . 'images/rooms/' . $room->image,
@@ -28,7 +45,7 @@ if ($request_method == 'GET') {
         }
     } else {
         http_response_code(404);
-        echo json_encode(['action' => 'error', 'message' => 'No rooms found']);
+        echo json_encode(['action' => 'error', 'message' => 'No rooms available for selected dates.']);
     }
     http_response_code(200);
     // Return the rooms data as JSON
